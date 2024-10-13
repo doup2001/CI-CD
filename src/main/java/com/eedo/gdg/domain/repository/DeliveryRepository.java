205a6bfdb344084650d10c6b51387350c533b26b
eedo_y <doup2001@naver.com>
2024-10-13 23:49:49 +0900

    fix : DeliveryRepository

--- src/main/java/com/eedo/gdg/domain/repository/DeliveryRepository.java
+++ src/main/java/com/eedo/gdg/domain/repository/DeliveryRepository.java
@@ -1,6 +1,7 @@
 package com.eedo.gdg.domain.repository;
 
+import com.eedo.gdg.domain.entity.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeliveryRepository extends JpaRepository {
+public interface DeliveryRepository extends JpaRepository<Delivery,Long> {
 }
