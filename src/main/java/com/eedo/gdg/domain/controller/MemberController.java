625d6de3a5be9fd5d8882f7efbc71386e2bd2b3e
eedo_y <doup2001@naver.com>
2024-10-13 23:49:28 +0900

    setting : controller Set

---
+++ src/main/java/com/eedo/gdg/domain/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.eedo.gdg.domain.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+public class MemberController {
+
+    @GetMapping()
+    public String hi() {
+        return " Member Hello";
+    }
+
+}
---
+++ src/main/java/com/eedo/gdg/domain/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.eedo.gdg.domain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    @GetMapping()
+    public String hi() {
+        return " Order Hello";
+    }
+}
