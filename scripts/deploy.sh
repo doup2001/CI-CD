aec723e57329d43d751bb974a28bef4da91723e1
eedo_y <doup2001@naver.com>
2024-11-06 11:45:42 +0900

    fix : yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
-echo "Starting deploy script"
 
-# Docker Compose 실행
-sudo /usr/local/bin/docker-compose --env-file .env -f ./docker-compose.yml up -d
+IS_GREEN_EXIST=$(docker ps | grep green)
+DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
-# 현재 실행 중인 프로세스를 확인하고 종료
-pid=$(pgrep -f gdg-0.0.1-SNAPSHOT.jar)
-if [ -n "${pid}" ]; then
-    kill -15 ${pid}
-    echo "Killed process ${pid}"
+# blue가 실행 중이면 green을 up합니다.
+if [ -z $IS_GREEN_EXIST ];then
+  echo "### BLUE => GREEN ####"
+  echo ">>> green image를 pull합니다."
+  docker-compose pull green
+  echo ">>> green container를 up합니다."
+  docker-compose up -d green
+  while [ 1 = 1 ]; do
+  echo ">>> green health check 중..."
+  sleep 3
+  REQUEST=$(curl http://127.0.0.1:8082)
+    if [ -n "$REQUEST" ]; then
+      echo ">>> 🍃 health check success !"
+      break;
+    fi
+  done;
+  sleep 3
+  echo ">>> nginx를 다시 실행 합니다."
+  sudo cp /etc/nginx/nginx.green.conf /etc/nginx/nginx.conf
+  sudo nginx -s reload
+  echo ">>> blue container를 down합니다."
+  docker-compose stop blue
+
+# green이 실행 중이면 blue를 up합니다.
 else
-    echo "No process found"
+  echo "### GREEN => BLUE ###"
+  echo ">>> blue image를 pull합니다."
+  docker-compose pull blue
+  echo ">>> blue container up합니다."
+  docker-compose up -d blue
+  while [ 1 = 1 ]; do
+    echo ">>> blue health check 중..."
+    sleep 3
+    REQUEST=$(curl http://127.0.0.1:8081)
+    if [ -n "$REQUEST" ]; then
+      echo ">>> 🍃 health check success !"
+      break;
+    fi
+  done;
+  sleep 3
+  echo ">>> nginx를 다시 실행 합니다."
+  sudo cp /etc/nginx/nginx.blue.conf /etc/nginx/nginx.conf
+  sudo nginx -s reload
+  echo ">>> green container를 down합니다."
+  docker-compose stop green
 fi
-
-# JAR 파일에 실행 권한 부여
-echo "Setting execute permission for JAR"
-chmod +x ./gdg-0.0.1-SNAPSHOT.jar
-
-# JAR 파일 실행
